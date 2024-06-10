0e1d6e710d452be684679eb86ef0b31f9e550a48
JC Tula <jc_tula@outlook.com>
2024-06-10 07:45:47 -0600

    Clase BL y primera parte del diseño del formulario

---
+++ src/main/java/interfazgrafica/ControlEstudiantes.java
@@ -0,0 +1,21 @@
+package interfazgrafica;
+
+import javax.swing.*;
+
+public class ControlEstudiantes {
+    private JPanel jpPrincipal;
+    private JTextField txtId;
+    private JTextField txtCodigo;
+    private JTextField txtNombre;
+    private JTextField txtApellido;
+    private JComboBox cbCarrera;
+    private JButton btnNuevo;
+    private JButton btnGuardar;
+    private JButton btnModificar;
+    private JButton btnEliminar;
+    private JButton btnCancelar;
+    private JButton btnSalir;
+    private JRadioButton rdbId;
+    private JRadioButton rdbApellido;
+    private JRadioButton rdbCarrera;
+}
